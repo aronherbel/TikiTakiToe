fb001bd8db35b40fda7f0b86be4c50f4a469bb3f
Aron Herbel <aron.herbel@ksb-sg.ch>
2023-09-27 17:10:12 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from "styled-components";
+import TikiTakiToki from "./TikiTakiToki";
+import "papercss/dist/paper.min.css";
+
+export default function App() {
+  return (
+    <Main>
+      <TikiTakiToki/>
+    </Main>
+  );
+}
+ 
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
